0193a6f3e4a43295a97e3ac09b8de514f530be3d
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-01-07 17:53:34 +1100

    Delete scripts/main/misc/testing.js

--- scripts/main/misc/testing.js
+++
@@ -1,9 +0,0 @@
-import { world } from "@minecraft/server";
-
-world.beforeEvents.playerInteractWithItem.subscribe((event) => {
-    const player = event.player;
-
-    if (item?.typeId === "minecraft:stick") {
-        player.sendMessage("You used a stick!");
-    }
-});
