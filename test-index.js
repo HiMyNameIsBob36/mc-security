4c935447f7fc142adf0a9b5bd4e83df2a099a935
HeyItsBob36 <166279266+HiMyNameIsBob36@users.noreply.github.com>
2026-01-07 17:46:06 +1100

    Update and rename old-index.js to test-index.js

--- test-index.js
+++ test-index.js
@@ -14,25 +14,17 @@ world.beforeEvents.chatSend.subscribe((event) => {
 system.runInterval(() => {
   tick++;
 
-  // Detect first time at least one player is in the world
-  if (!worldLoaded) {
-    const players = world.getPlayers();
-
-    if (players.length > 0) {
-      world.sendMessage(`World has loaded in ${tick} ticks`);
-      worldLoaded = true;
-
       // Create scoreboard objective if it doesn't exist yet
       try {
         world.scoreboard.addObjective("chatsSent", "chatsSent");
       } catch (e) {
         // Ignore error if the objective already exists
       }
-    }
-  }
+
 
   // Call timer() once per second (20 ticks)
   if (system.currentTick % 20 === 0) {
     timer();
   }
 }, 1);
+
